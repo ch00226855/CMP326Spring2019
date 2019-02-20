d5adb91b6de38a049440caad6d6185969a8f619e
ch00226855 <ch00226855@gmail.com>
2019-02-20 08:59:17 -0500

    Person, Employee, Company class

---
+++ CMP326Ch9/Person.java
@@ -0,0 +1,33 @@
+public class Person{
+	
+	private String name;
+	private int birthYear;
+	private double weight;
+	private char gender;
+
+	public Person(String name, int birthYear, double weight, char gender){
+		this.name = name;
+		this.birthYear = birthYear;
+
+		if (weight > 0.0){
+			this.weight = weight;
+		} else {
+			this.weight = 0.0;
+		}
+
+		if (gender == 'f' || gender == 'm' || gender == 'u'){
+			this.gender = gender;
+		} else { // 'u' for unknown
+			this.gender = 'u';
+		}
+	}
+
+	public boolean isMale(){
+		return (gender == 'm');
+	}
+
+	public void printDetails(){
+		System.out.printf("Name: %20s | Year of Birth: %4d | Weight: %10.2f | Gender: %c\n", name, birthYear, weight, gender);
+	}
+
+}
---
+++ README
@@ -0,0 +1,7 @@
+## CMP 326 Class Materials
+
+This is a repository for storing programs created in CMP 326 class.
+
+### Contents
+
+- CMP326Ch9: Class Person, Employee, Company
